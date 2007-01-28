4aa406603b2302a99fc84603a5281314293f6c9d
evan <evan@d8a25685-d327-0410-a4bd-b5481dd33b1d>
2007-01-28 22:33:23 +0000

    Set an ignore.
    git-svn-id: http://mindsplatter.net/opensprints@8 d8a25685-d327-0410-a4bd-b5481dd33b1d

--- trunk/ror/app/controllers/irosprints_controller.rb
+++ trunk/ror/app/controllers/irosprints_controller.rb
@@ -1,5 +1,12 @@
 class IrosprintsController < ApplicationController
+  RED_TRACK_LENGTH = 1315
+  BLUE_TRACK_LENGTH = 1200
+# 1km = 100,000 centimeters
   def index
-    @pointer_angle = rand(180+90)-180 
+    @pointer_angle = rand(180+90)-180
+    @laps = 3
+    @red_dasharray = [1315,10e12]
+    @blue_dasharray = [1200,10e12]
+    [@red_dasharray,@blue_dasharray].map!{|da|da.join(',')}
   end
 end
