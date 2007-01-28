e7ffaea58bb4faa0a6513239f3b5d1434c6d0f68
evan <evan@d8a25685-d327-0410-a4bd-b5481dd33b1d>
2007-01-28 08:06:33 +0000

    Somewhat implemented CSS. Made the dial workable.
    git-svn-id: http://mindsplatter.net/opensprints@7 d8a25685-d327-0410-a4bd-b5481dd33b1d

--- trunk/ror/app/controllers/irosprints_controller.rb
+++ trunk/ror/app/controllers/irosprints_controller.rb
@@ -1,24 +1,5 @@
 class IrosprintsController < ApplicationController
-  def poly
-    do_math
-  end
-  def draw_a_polygon
-    do_math
-    render :partial => 'draw_a_polygon'
-  end
-private
-
-  def do_math
-    sides = random_number_over_two
-    str = ""
-    @polystring = (1..sides).map do |s| 
-      str << "#{(Math::cos(2*Math::PI/sides*s)+1)*70} ,#{(Math::sin(2*Math::PI/sides*s)+1)*70} "
-    end
-    @polystring = str
-  end
-
-  def random_number_over_two
-    @sides = Time.now.to_f.to_s.split(//).last.to_i
-    @sides > 2 ? @sides : random_number_over_two
+  def index
+    @pointer_angle = rand(180+90)-180 
   end
 end
