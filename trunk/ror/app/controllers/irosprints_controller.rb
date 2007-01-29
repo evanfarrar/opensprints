cd9349e45c4f9a4df49ed06a0e92cbe35b471a27
evan <evan@d8a25685-d327-0410-a4bd-b5481dd33b1d>
2007-01-29 00:28:02 +0000

    More track changes. New dial numbering scheme.
    git-svn-id: http://mindsplatter.net/opensprints@9 d8a25685-d327-0410-a4bd-b5481dd33b1d

--- trunk/ror/app/controllers/irosprints_controller.rb
+++ trunk/ror/app/controllers/irosprints_controller.rb
@@ -3,10 +3,29 @@ class IrosprintsController < ApplicationController
   BLUE_TRACK_LENGTH = 1200
 # 1km = 100,000 centimeters
   def index
-    @pointer_angle = rand(180+90)-180
+    @dial_90_degrees = 8
+    @dial_180_degrees = 24
+    @dial_270_degrees = 40
+    @pointer_angle = speed_to_angle(rand(48))
     @laps = 3
-    @red_dasharray = [1315,10e12]
-    @blue_dasharray = [1200,10e12]
-    [@red_dasharray,@blue_dasharray].map!{|da|da.join(',')}
+    @red_dasharray = quadrantificate(765,1315,rand(1315))
+    @blue_dasharray = quadrantificate(700,1200,rand(1200))
+    @blue_dasharray = @blue_dasharray.join(',')
+    @red_dasharray = @red_dasharray.join(',')
+  end
+
+private
+  def quadrantificate(offset=700, total=1200, distance=0)
+# over 700 
+    if distance > offset
+      [0,0,offset,((total-offset)-(distance-offset))]
+    else
+      [0,(offset-distance),distance,(total-offset)]
+    end
+  end
+
+  def speed_to_angle(speed)
+    unadjusted = ((speed/48.0)*270.0+45.0)
+    unadjusted-180
   end
 end
