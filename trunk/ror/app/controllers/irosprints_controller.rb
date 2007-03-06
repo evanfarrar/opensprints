069a5b50250f3a23816e67743212463f50e5147c
efarrar <efarrar@d8a25685-d327-0410-a4bd-b5481dd33b1d>
2007-03-06 06:40:23 +0000

    WOOT. Static speed/distance rendering basically working.
    git-svn-id: http://mindsplatter.net/opensprints@13 d8a25685-d327-0410-a4bd-b5481dd33b1d

--- trunk/ror/app/controllers/irosprints_controller.rb
+++ trunk/ror/app/controllers/irosprints_controller.rb
@@ -6,10 +6,10 @@ class IrosprintsController < ApplicationController
     @dial_90_degrees = 8
     @dial_180_degrees = 24
     @dial_270_degrees = 40
+    read_log
+    read_red
     @blue_pointer_angle = speed_to_angle(rand(48))
-    @red_pointer_angle = speed_to_angle(rand(48))
     @laps = 3
-    @red_dasharray = quadrantificate(765,1315,rand(1315))
     @blue_dasharray = quadrantificate(700,1200,rand(1200))
     @blue_dasharray = @blue_dasharray.join(',')
     @red_dasharray = @red_dasharray.join(',')
@@ -28,5 +28,22 @@ private
   def speed_to_angle(speed)
     unadjusted = ((speed/48.0)*270.0+45.0)
     unadjusted-180
+  end
+
+  def read_log
+    @log = File.read('sample.dat'){|f| f.readlines}
+  end
+
+  def read_red
+    a = @log.select{|l|l=~/one-tick/}
+    distance = (a.length*0.7)
+    @red_dasharray =quadrantificate(765, RED_TRACK_LENGTH, distance)
+    last = YAML::load(a[-2])['rider-one-tick']
+    this = YAML::load(a[-1])['rider-one-tick']
+    @red_pointer_angle = speed_to_angle(rotation_elapsed_to_kmh(this-last))
+  end
+
+  def rotation_elapsed_to_kmh(elapsed)
+    ((700.0/(elapsed))/(1000.0*100.0))*3600.0
   end
 end
--- trunk/ror/lib/sensor.rb
+++ trunk/ror/lib/sensor.rb
@@ -1,12 +1,15 @@
 require 'serialport.so'
 
-sp = SerialPort.new('/dev/ttyUSB0', 9600, 8, 1, SerialPort::NONE)
-
-open("log/serial.log", "r+") { |log|
-  while true do
-    if 
-    log.puts sp.getc
+sp = SerialPort.new('/dev/ttyUSB0', 115200, 8, 1, SerialPort::NONE)
+t_start = Time.now.to_f
+t_then = t_start
+t_now = t_start
+while true do
+  if sp.gets
+    t_now = Time.now.to_f
+    puts "rider-one-tick: #{t_now-t_start}" if (t_now-t_then)>0.05
   end
-}
+  t_then = t_now
+end
 
 sp.close
