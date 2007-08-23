90d717d31120a08bd87a6f8ca51d45660284d479
efarrar <efarrar@d8a25685-d327-0410-a4bd-b5481dd33b1d>
2007-08-23 17:42:21 +0000

    added some keys to start and stop a race, and also deleted a silly file.
    git-svn-id: http://mindsplatter.net/opensprints@55 d8a25685-d327-0410-a4bd-b5481dd33b1d

--- trunk/opensprints/main.rb
+++ trunk/opensprints/main.rb
@@ -16,30 +16,44 @@ BLUE_WHEEL_CIRCUMFERENCE = 85.mm.to_km
 @w.title = "IRO Sprints"
 @w.resize(993, 741)
 box = Gtk::VBox.new(false, 0)
-dashboard_controller = DashboardController.new
+@dashboard_controller = DashboardController.new
 rpb = RSVG::Handle.new_from_data('<svg></svg>')
-gi = Gtk::Image.new(rpb.pixbuf)
-countdown = 5
-Gtk.timeout_add(1000) do
-  case countdown
-  when (1..10)
-    gi.pixbuf=dashboard_controller.count(countdown)
-    countdown-=1
-    true
-  when 0
-    dashboard_controller.begin_logging
-    Gtk.timeout_add(100) do
-      gi.pixbuf=dashboard_controller.refresh
-      dashboard_controller.continue?
+@gi = Gtk::Image.new(rpb.pixbuf)
+def start_race
+  countdown = 5
+  @gi.pixbuf=@dashboard_controller.count(countdown)
+  @timeout = Gtk.timeout_add(1000) do
+    case countdown
+    when (1..10)
+      @gi.pixbuf=@dashboard_controller.count(countdown)
+      countdown-=1
+      true
+    when 0
+      @dashboard_controller.begin_logging
+      @timeout = Gtk.timeout_add(100) do
+        @gi.pixbuf=@dashboard_controller.refresh
+        @dashboard_controller.continue?
+      end
+      false    
     end
-    false    
   end
 end
 @w.signal_connect("destroy") do
-  `killall -9 ruby`
   Gtk.main_quit
 end
-box.pack_start(gi)
+@w.signal_connect("key_press_event") do |window,event|
+  if event.keyval == ?a
+    stop_race
+    start_race
+  elsif event.keyval == ?z
+    stop_race
+  end
+end
+def stop_race
+  Gtk.timeout_remove(@timeout) if @timeout
+  @dashboard_controller.t.kill if @dashboard_controller.t
+end
+box.pack_start(@gi)
 @w << box
 @w.show_all
 Gtk.main
