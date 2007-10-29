772f6be88d14033395b4413530d20e8c8c0c53d7
efarrar <efarrar@d8a25685-d327-0410-a4bd-b5481dd33b1d>
2007-10-29 22:19:03 +0000

    Rolled back to a pre-Rubygame version.
    git-svn-id: http://mindsplatter.net/opensprints@72 d8a25685-d327-0410-a4bd-b5481dd33b1d

--- trunk/opensprints/lib/dashboard_controller.rb
+++ trunk/opensprints/lib/dashboard_controller.rb
@@ -1,52 +1,60 @@
 require 'thread'
-
 class DashboardController
   def initialize
+    style
+    @dial_90_degrees = 8
+    @dial_180_degrees = 24
+    @dial_270_degrees = 40
     @red = Racer.new(:wheel_circumference => RED_WHEEL_CIRCUMFERENCE,
                      :track_length => 1315, :yaml_name => '1')
     @blue = Racer.new(:wheel_circumference => BLUE_WHEEL_CIRCUMFERENCE,
                     :track_length => 1315, :yaml_name => '2')
     @laps = 1
     @continue = true
-    @surface = Rubygame::Surface.new([794,614])
-    @surface.fill([61,52,53])
-#    @surface = Rubygame::Surface.load_image('views/mockup.jpg')
-    @chris_awesome_font = Rubygame::TTF.new('views/DINMittelschriftStd.otf', 60)
-    @chris_awesome_font2 = Rubygame::TTF.new('views/DINEngschriftStd.otf', 115)
-    @chris_awesome_font.render('IRO', true, [254,240,2]).blit(@surface,[70,47]) 
-    @chris_awesome_font.render('Sprints', true, [255,255,255]).blit(@surface,[157,47])
-# "gradient"
-    h = 2
-    (1..50).each do |n|
-      @surface.draw_box_s([0, 613-((n*h)-h)], [794, 614-(n*h)], [252,252,252, (255-(255*(n/50.0)))*0.5])
+    @doc = build_template
+    @svg = RSVG::Handle.new
+  end
+  def quadrantificate(offset, total, distance=0)
+    distance*=1/RACE_DISTANCE
+    if distance > offset
+      [0,0,offset,((total-offset)-(distance-offset))]
+    else
+      [0,(offset-distance),distance,(total-offset)]
     end
-
-#title underline
-    @surface.draw_box_s([30, 97], [187, 97], [252,252,252])
-    @surface.draw_box_s([210, 97], [740, 97], [252,252,252])
-#start/end labels
-    @surface.draw_box_s([27, 129], [44, 318], [203,195,192])
-    @surface.draw_box_s([727, 129], [745, 318], [203,195,192])
-#progress borders
-    @surface.draw_box_s([27, 129], [745, 129], [203,195,192])
-    @surface.draw_box_s([27, 318], [745, 318], [203,195,192])
-#statboxes
-    @surface.draw_box_s([27, 357], [253, 456], [165,86,64])
-    @surface.draw_box_s([269, 357], [495, 456], [77,134,161])
-#nameboxes
-    @surface.draw_box_s([27, 332], [253, 357], [207,95,55])
-    @surface.draw_box_s([269, 332], [495, 357], [65,167,207])
-#message box
-    @surface.draw_box([27, 471], [745, 559], [203,195,192])
-
-    @last_blue_tick = [45,318]
-    @last_red_tick = [45,318]
   end
-  
-  def read
+  def read_blue
+    blue_log = @partial_log.grep(/^2/)
+    if blue_log
+      @blue.update(blue_log)
+      track = BLUE_TRACK_LENGTH*@blue.distance
+      @blue_dasharray = quadrantificate(700, BLUE_TRACK_LENGTH, track).join(',')
+    end
   end
- 
-  def start
+  def read_red
+    red_log = @partial_log.grep(/^1/)
+    if red_log
+      @red.update(red_log)
+      track = RED_TRACK_LENGTH*@red.distance
+      @red_dasharray = quadrantificate(765, RED_TRACK_LENGTH, track).join(',')
+    end
+  end
+  def style
+    File.open('views/style.css') do |f|
+      @stylishness = f.readlines.join
+    end
+  end
+
+  def build_template
+    xml_data = ''
+    xml = Builder::XmlMarkup.new(:target => xml_data)
+    svg = ''
+    File.open('views/svg.rb') do |f|
+      svg = f.readlines.join
+    end
+    eval svg
+    xml_data.gsub!(/%([^s])/,'%%\1')
+  end
+  def begin_logging
     @queue = Queue.new
     @sensor = Sensor.new(@queue)
     @sensor.start
@@ -56,53 +64,46 @@ class DashboardController
     @sensor.stop
   end
 
-  def update
-    partial_log = []
+  def refresh
+    @partial_log = []
     @queue.length.times do
-      partial_log << @queue.pop
+      @partial_log << @queue.pop
     end
-    if partial_log.any?
-      if (blue_log = partial_log.grep(/^2/))
-        @blue.update(blue_log)
-      end
-      if (red_log = partial_log.grep(/^1/))
-        @red.update(red_log)
-      end
+    if @partial_log.any?
+      read_red
+      read_blue
       if @blue.distance>RACE_DISTANCE or @red.distance>RACE_DISTANCE
+        puts @blue.tix
         winner = (@red.distance>@blue.distance) ? 'RED' : 'BLUE'
-        @sensor.stop
-        @chris_awesome_font.render("#{winner} WINS!", true, [255,255,255]).blit(@surface, [0,0])
+        svg = RSVG::Handle.new_from_data(@doc % [0,0,"#{winner} WINS!!!",4,5,6])
+        svg.base_uri = Dir.pwd
+        @continue = false
+      else
+        svg = RSVG::Handle.new_from_data(@doc % [@red_dasharray,
+                        @blue_dasharray, TITLE])
+        svg.base_uri = Dir.pwd
+        @continue = true
       end
-      blue_progress = 683*@blue.percent_complete
-      @surface.draw_box_s([45, 150], [45+blue_progress, 170],  [54,127,155])
-      @surface.draw_box_s([269, 357], [495, 456], [77,134,161])
-      @chris_awesome_font2.render("%.0f" % @blue.speed, true,[65,167,207]).blit(@surface, [279,365])
-      
-      red_progress = 683*@red.percent_complete
-      @surface.draw_box_s([45, 129], [45+red_progress, 149],[159,77,56])
-      @surface.draw_box_s([27, 357], [253, 456], [165,86,64])
-      @chris_awesome_font2.render("%.0f" % @red.speed, true, [207,95,55]).blit(@surface, [37,365])
-    
-      @surface.draw_box_s([27, 129], [745, 129], [203,195,192])
-      @surface.draw_box_s([27, 150], [745, 150], [203,195,192])
-      @surface.draw_box_s([27, 171], [745, 171], [203,195,192])
-
-      tick_at = graph_tick(@red.distance, @red.speed)
-      @surface.draw_polygon_s([ @last_red_tick.map{|e|e+3}, @last_red_tick, tick_at, tick_at.map{|e|e+2}], [159,77,56])
-      @last_red_tick = tick_at
-
-
-      tick_at = graph_tick(@blue.distance, @blue.speed)
-      @surface.draw_polygon_s([ @last_blue_tick.map{|e|e+3}, @last_blue_tick, tick_at, tick_at.map{|e|e+2}], [54,127,155])
-      @last_blue_tick = tick_at
+      @pix = svg ? svg.pixbuf : nil
     end
-    @surface
+    @pix||RSVG::Handle.new_from_data(@doc % [0,0,"0...",0,0,4,5,6]).pixbuf
+  end
+  def continue?
+    @continue
+  end
+  def count(n)
+    svg = RSVG::Handle.new_from_data(@doc % [0,0,"#{n}...",0,0,4,5,6])
+    svg.pixbuf
   end
 
-  def graph_tick(distance, speed)
-    [(distance/RACE_DISTANCE.to_f*(726-45) + 45),
-     (147 - ([speed,50.0].min/50.0 * 147) + 171)]
+  def background
+    xml_data = ''
+    xml = Builder::XmlMarkup.new(:target => xml_data)
+    svg = ''
+    File.open('views/svg.rb') do |f|
+      svg = f.readlines.join
+    end
+    eval svg
+    xml_data
   end
-
-
 end
--- trunk/opensprints/lib/racer.rb
+++ trunk/opensprints/lib/racer.rb
@@ -2,7 +2,6 @@ require 'units/standard'
 #require 'yaml'
 class Racer
   attr_accessor :distance
-  attr :speed
   attr :wheel_circumference
   attr :yaml_name
   def initialize(attributes = {})
@@ -14,40 +13,13 @@ class Racer
   end
 
   def update(new_ticks)
-    @ticks += new_ticks.map{|t| t.split(/: /)[1].to_f}
+    @ticks += new_ticks
     ticks_length = @ticks.length
     @distance = ((ticks_length)*(@wheel_circumference))
-    if ticks_length>5
-      diffs = []
-      last = @ticks[-6..-1]
-      last.each_with_index{|e,i| 
-        (diffs<<(last[i+1]-e)) if last[i+1]
-      }
-      ave_elapsed = (diffs.inject(0){|acc,n| acc=acc+n})/5.0
-      @speed = rotation_elapsed_to_kmh(ave_elapsed)
-    elsif ticks_length>1
-      this = @ticks[-1]
-      last = @ticks[-2]
-      @speed = rotation_elapsed_to_kmh(this-last)
-    else
-      @speed = 0
-    end
   end
 
   def tix
     @ticks.length
-  end
-  
-  def speed
-    unless @speed == 1/0.0
-      @speed
-    else
-      0
-    end
-  end
-
-  def percent_complete
-    @ticks.length*@wheel_circumference/RACE_DISTANCE.to_f
   end
 
 private
