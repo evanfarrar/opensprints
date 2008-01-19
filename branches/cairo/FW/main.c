f9c9da372bec8b2c60c6eface755d6c18b610db0
efarrar <efarrar@d8a25685-d327-0410-a4bd-b5481dd33b1d>
2008-01-19 02:45:23 +0000

    Whoops didnt get it all in
    git-svn-id: http://mindsplatter.net/opensprints@102 d8a25685-d327-0410-a4bd-b5481dd33b1d

---
+++ branches/cairo/FW/main.c
@@ -0,0 +1,152 @@
+/*********************************************************************
+ *
+ *                Microchip USB C18 Firmware Version 1.0
+ *
+ *********************************************************************
+ * FileName:        main.c
+ * Dependencies:    See INCLUDES section below
+ * Processor:       PIC18
+ * Compiler:        C18 2.30.01+
+ * Company:         Microchip Technology, Inc.
+ *
+ * Software License Agreement
+ *
+ * The software supplied herewith by Microchip Technology Incorporated
+ * (the “Company”) for its PICmicro® Microcontroller is intended and
+ * supplied to you, the Company’s customer, for use solely and
+ * exclusively on Microchip PICmicro Microcontroller products. The
+ * software is owned by the Company and/or its supplier, and is
+ * protected under applicable copyright laws. All rights are reserved.
+ * Any use in violation of the foregoing restrictions may subject the
+ * user to criminal sanctions under applicable laws, as well as to
+ * civil liability for the breach of the terms and conditions of this
+ * license.
+ *
+ * THIS SOFTWARE IS PROVIDED IN AN “AS IS” CONDITION. NO WARRANTIES,
+ * WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT NOT LIMITED
+ * TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. THE COMPANY SHALL NOT,
+ * IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL OR
+ * CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+ *
+ * Author               Date        Comment
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Rawin Rojvanit       11/19/04    Original.
+ ********************************************************************/
+
+/** I N C L U D E S **********************************************************/
+#include <p18cxxx.h>
+#include "system\typedefs.h"                        // Required
+#include "system\usb\usb.h"                         // Required
+#include "io_cfg.h"                                 // Required
+
+#include "system\usb\usb_compile_time_validation.h" // Optional
+#include "user\user.h"                              // Modifiable
+
+/** V A R I A B L E S ********************************************************/
+#pragma udata
+
+/** P R I V A T E  P R O T O T Y P E S ***************************************/
+static void InitializeSystem(void);
+void USBTasks(void);
+
+#pragma code high_vector_section = 0x000808
+void high_vector (void)
+{
+    _asm goto high_ISR _endasm
+}
+#pragma code low_vector_section = 0x000818
+void low_vector (void)
+{
+    _asm goto low_ISR _endasm
+}
+#pragma code
+
+/** D E C L A R A T I O N S **************************************************/
+#pragma code
+/******************************************************************************
+ * Function:        void main(void)
+ *
+ * PreCondition:    None
+ *
+ * Input:           None
+ *
+ * Output:          None
+ *
+ * Side Effects:    None
+ *
+ * Overview:        Main program entry point.
+ *
+ * Note:            None
+ *****************************************************************************/
+void main(void)
+{
+    InitializeSystem();
+	while(1)
+    {
+        USBTasks();         // USB Tasks
+        ProcessIO();        // See user.c and user.h
+    }//end while
+}//end main
+
+/******************************************************************************
+ * Function:        static void InitializeSystem(void)
+ *
+ * PreCondition:    None
+ *
+ * Input:           None
+ *
+ * Output:          None
+ *
+ * Side Effects:    None
+ *
+ * Overview:        InitializeSystem is a centralize initialization routine.
+ *                  All required USB initialization routines are called from
+ *                  here.
+ *
+ *                  User application initialization routine should also be
+ *                  called from here.                  
+ *
+ * Note:            None
+ *****************************************************************************/
+static void InitializeSystem(void)
+{
+    ADCON1 |= 0x0F;                 // Default all pins to digital
+        
+    mInitializeUSBDriver();         // See usbdrv.h
+    
+    UserInit();                     // See user.c & .h
+
+}//end InitializeSystem
+
+/******************************************************************************
+ * Function:        void USBTasks(void)
+ *
+ * PreCondition:    InitializeSystem has been called.
+ *
+ * Input:           None
+ *
+ * Output:          None
+ *
+ * Side Effects:    None
+ *
+ * Overview:        Service loop for USB tasks.
+ *
+ * Note:            None
+ *****************************************************************************/
+void USBTasks(void)
+{
+    /*
+     * Servicing Hardware
+     */
+    USBCheckBusStatus();                    // Must use polling method
+    if(UCFGbits.UTEYE!=1)
+        USBDriverService();                 // Interrupt or polling method
+    
+    #if defined(USB_USE_CDC)
+    CDCTxService();
+    #endif
+
+}// end USBTasks
+
+/** EOF main.c ***************************************************************/
---
+++ branches/cairo/lib/secsy_time.rb
@@ -0,0 +1,23 @@
+class SecsyTime
+  attr :mins
+  attr :secs
+  attr :hunds
+
+  def initialize(mins,secs,hunds)
+    @mins = mins
+    @secs = secs
+    @hunds = hunds
+  end
+
+  def SecsyTime.parse(str)
+    return SecsyTime.new(0,0,0) unless str
+    mins,secshunds = str.split(':')
+    four_chars = '%04i' % secshunds
+    secs,hunds = four_chars[0..1].to_i,four_chars[2..3].to_i
+    SecsyTime.new(mins.to_i,secs,hunds)
+  end
+
+  def in_seconds
+    @mins.to_i * 60 + @secs + @hunds.to_i/100.0
+  end
+end
