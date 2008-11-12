af79854dfdfedaab41dc0c2a9dafc1c8abd0d2e0
Evan Farrar <evanfarrar@gmail.com>
2008-11-11 19:26:43 -0600

    Just added a 'tournament' button to the main menu

--- main.rb
+++ main.rb
@@ -7,7 +7,13 @@ Shoes.app do
       load 'lib/config_app.rb'
     end
 
-    button "race with names" 
+    button "race with names" do
+      alert('not implemented!')
+    end
+
+    button "race a tournament" do
+      alert('not implemented!')
+    end
 
     button "Just Race!" do
       load 'lib/race_app.rb'
