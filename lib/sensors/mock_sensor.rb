b0847e8ff8b42bc99e1fcc3b19e6ecb0e695c7a1
Evan Farrar <evanfarrar@gmail.com>
2008-11-13 20:14:30 -0600

    Modified the mock sensor to work.

--- lib/sensors/mock_sensor.rb
+++ lib/sensors/mock_sensor.rb
@@ -1,39 +1,33 @@
-#mock_sensor: a sensor that you can use without real sensors attached
+#Arduino: a sensor written for the arduino open source hardware.
 class Sensor
-  attr_accessor :queue
+  attr_accessor :queue, :r
   def initialize(queue, filename=nil)
-    @queue = queue
-    @filename = nil
   end
 
   def start
-    @queue.clear
-    @t.kill if @t
+    @start = Time.now
     @t = Thread.new do
-      t = 0
-      f = []
-      8000.times { f << "#{rand(2)+1}: #{t+=rand(100)}" }
-      t_start = Time.now.to_f
-      while true do
-        l = f.shift
-        sleeptime = l.split[1].to_f/1000.0 - (Time.now.to_f-t_start)
-        sleep sleeptime if sleeptime > 0
-        @queue << l 
-        puts l
+      loop do
+        fake = sixty_times_a_sec
+        Thread.current["vals"] = {
+                                   :red => fake, 
+                                   :blue => fake,
+                                   :green => fake,
+                                   :yellow => fake
+                                 }
       end
     end
- 
-    self
+  end
+
+  def values
+    @t["vals"]
+  end
+
+  def sixty_times_a_sec
+    [(Time.now - @start)*1000] * (Time.now - @start)*60
   end
 
   def stop
     @t.kill
-  end
-
-end
-
-class Float
-  def naturalize
-    self > 0 ? self : 0
   end
 end
