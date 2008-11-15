f98e9afe30cdcaddfc343aefa9e9b17c17028e66
Luke Orland <orluke@gmail.com>
2008-11-15 01:13:29 -0600

    interface board: interface board layout created with all components,  * 'border' layer showing the pcb edges.  * new RJ45 package added from the internets  * need to create a package for the headers that the arduino connects    to.

---
+++ hardware/interface_board/geda/interface_board.cmd
@@ -0,0 +1,127 @@
+# Pin name action command file
+
+# Start of element CONN4
+ChangePinName(CONN4, 7, 7)
+ChangePinName(CONN4, 5, 5)
+ChangePinName(CONN4, 3, 3)
+ChangePinName(CONN4, 1, 1)
+ChangePinName(CONN4, 8, 8)
+ChangePinName(CONN4, 6, 6)
+ChangePinName(CONN4, 4, 4)
+ChangePinName(CONN4, 2, 2)
+
+# Start of element R4
+ChangePinName(R4, 1, 1)
+ChangePinName(R4, 2, 2)
+
+# Start of element CONN3
+ChangePinName(CONN3, 8, I7)
+ChangePinName(CONN3, 7, I6)
+ChangePinName(CONN3, 6, I5)
+ChangePinName(CONN3, 5, I4)
+ChangePinName(CONN3, 4, I3)
+ChangePinName(CONN3, 3, I2)
+ChangePinName(CONN3, 2, I1)
+ChangePinName(CONN3, 1, I0)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 2, 2)
+
+# Start of element CONN2
+ChangePinName(CONN2, 7, 7)
+ChangePinName(CONN2, 5, 5)
+ChangePinName(CONN2, 3, 3)
+ChangePinName(CONN2, 1, 1)
+ChangePinName(CONN2, 8, 8)
+ChangePinName(CONN2, 6, 6)
+ChangePinName(CONN2, 4, 4)
+ChangePinName(CONN2, 2, 2)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element CONN1
+ChangePinName(CONN1, 8, I7)
+ChangePinName(CONN1, 7, I6)
+ChangePinName(CONN1, 6, I5)
+ChangePinName(CONN1, 5, I4)
+ChangePinName(CONN1, 4, I3)
+ChangePinName(CONN1, 3, I2)
+ChangePinName(CONN1, 2, I1)
+ChangePinName(CONN1, 1, I0)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element CONN8
+ChangePinName(CONN8, 7, 7)
+ChangePinName(CONN8, 5, 5)
+ChangePinName(CONN8, 3, 3)
+ChangePinName(CONN8, 1, 1)
+ChangePinName(CONN8, 8, 8)
+ChangePinName(CONN8, 6, 6)
+ChangePinName(CONN8, 4, 4)
+ChangePinName(CONN8, 2, 2)
+
+# Start of element CONN7
+ChangePinName(CONN7, 8, I7)
+ChangePinName(CONN7, 7, I6)
+ChangePinName(CONN7, 6, I5)
+ChangePinName(CONN7, 5, I4)
+ChangePinName(CONN7, 4, I3)
+ChangePinName(CONN7, 3, I2)
+ChangePinName(CONN7, 2, I1)
+ChangePinName(CONN7, 1, I0)
+
+# Start of element CONN6
+ChangePinName(CONN6, 7, 7)
+ChangePinName(CONN6, 5, 5)
+ChangePinName(CONN6, 3, 3)
+ChangePinName(CONN6, 1, 1)
+ChangePinName(CONN6, 8, 8)
+ChangePinName(CONN6, 6, 6)
+ChangePinName(CONN6, 4, 4)
+ChangePinName(CONN6, 2, 2)
+
+# Start of element U1
+ChangePinName(U1, 20, DIGIO7)
+ChangePinName(U1, 19, DIGIO6)
+ChangePinName(U1, 18, DIGIO5)
+ChangePinName(U1, 17, DIGIO4)
+ChangePinName(U1, 16, DIGIO3)
+ChangePinName(U1, 15, DIGIO2)
+ChangePinName(U1, 14, DIGIO1 (TX))
+ChangePinName(U1, 21, DIGIO8)
+ChangePinName(U1, 22, DIGIO9)
+ChangePinName(U1, 23, DIGIO10)
+ChangePinName(U1, 24, DIGIO11)
+ChangePinName(U1, 13, DIGIO0 (RX))
+ChangePinName(U1, 27, GND)
+ChangePinName(U1, 28, AREF)
+ChangePinName(U1, 12, AIN5)
+ChangePinName(U1, 11, AIN4)
+ChangePinName(U1, 10, AIN3)
+ChangePinName(U1, 9, AIN2)
+ChangePinName(U1, 8, AIN1)
+ChangePinName(U1, 7, AIN0)
+ChangePinName(U1, 6, Vin)
+ChangePinName(U1, 25, DIGIO12)
+ChangePinName(U1, 26, DIGIO13)
+ChangePinName(U1, 5, GND)
+ChangePinName(U1, 4, GND)
+ChangePinName(U1, 3, 5V)
+ChangePinName(U1, 2, 3V3)
+ChangePinName(U1, 1, RESET)
+
+# Start of element CONN5
+ChangePinName(CONN5, 8, I7)
+ChangePinName(CONN5, 7, I6)
+ChangePinName(CONN5, 6, I5)
+ChangePinName(CONN5, 5, I4)
+ChangePinName(CONN5, 4, I3)
+ChangePinName(CONN5, 3, I2)
+ChangePinName(CONN5, 2, I1)
+ChangePinName(CONN5, 1, I0)
