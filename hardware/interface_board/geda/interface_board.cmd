c3d0035b931ba35e3b5156700e53f0e70914426d
Luke Orland <orluke@gmail.com>
2008-11-16 15:01:35 -0600

    interface board: changed refdes on connectors and RJ45 jacks

--- hardware/interface_board/geda/interface_board.cmd
+++ hardware/interface_board/geda/interface_board.cmd
@@ -10,23 +10,15 @@ ChangePinName(CONN4, 6, 6)
 ChangePinName(CONN4, 4, 4)
 ChangePinName(CONN4, 2, 2)
 
-# Start of element R4
-ChangePinName(R4, 1, 1)
-ChangePinName(R4, 2, 2)
-
 # Start of element CONN3
-ChangePinName(CONN3, 8, I7)
-ChangePinName(CONN3, 7, I6)
-ChangePinName(CONN3, 6, I5)
-ChangePinName(CONN3, 5, I4)
-ChangePinName(CONN3, 4, I3)
-ChangePinName(CONN3, 3, I2)
-ChangePinName(CONN3, 2, I1)
-ChangePinName(CONN3, 1, I0)
-
-# Start of element R3
-ChangePinName(R3, 1, 1)
-ChangePinName(R3, 2, 2)
+ChangePinName(CONN3, 7, 7)
+ChangePinName(CONN3, 5, 5)
+ChangePinName(CONN3, 3, 3)
+ChangePinName(CONN3, 1, 1)
+ChangePinName(CONN3, 8, 8)
+ChangePinName(CONN3, 6, 6)
+ChangePinName(CONN3, 4, 4)
+ChangePinName(CONN3, 2, 2)
 
 # Start of element CONN2
 ChangePinName(CONN2, 7, 7)
@@ -38,53 +30,55 @@ ChangePinName(CONN2, 6, 6)
 ChangePinName(CONN2, 4, 4)
 ChangePinName(CONN2, 2, 2)
 
-# Start of element R2
-ChangePinName(R2, 1, 1)
-ChangePinName(R2, 2, 2)
-
 # Start of element CONN1
-ChangePinName(CONN1, 8, I7)
-ChangePinName(CONN1, 7, I6)
-ChangePinName(CONN1, 6, I5)
-ChangePinName(CONN1, 5, I4)
-ChangePinName(CONN1, 4, I3)
-ChangePinName(CONN1, 3, I2)
-ChangePinName(CONN1, 2, I1)
-ChangePinName(CONN1, 1, I0)
+ChangePinName(CONN1, 7, 7)
+ChangePinName(CONN1, 5, 5)
+ChangePinName(CONN1, 3, 3)
+ChangePinName(CONN1, 1, 1)
+ChangePinName(CONN1, 8, 8)
+ChangePinName(CONN1, 6, 6)
+ChangePinName(CONN1, 4, 4)
+ChangePinName(CONN1, 2, 2)
 
-# Start of element R1
-ChangePinName(R1, 1, 1)
-ChangePinName(R1, 2, 2)
+# Start of element J4
+ChangePinName(J4, 8, I7)
+ChangePinName(J4, 7, I6)
+ChangePinName(J4, 6, I5)
+ChangePinName(J4, 5, I4)
+ChangePinName(J4, 4, I3)
+ChangePinName(J4, 3, I2)
+ChangePinName(J4, 2, I1)
+ChangePinName(J4, 1, I0)
 
-# Start of element CONN8
-ChangePinName(CONN8, 7, 7)
-ChangePinName(CONN8, 5, 5)
-ChangePinName(CONN8, 3, 3)
-ChangePinName(CONN8, 1, 1)
-ChangePinName(CONN8, 8, 8)
-ChangePinName(CONN8, 6, 6)
-ChangePinName(CONN8, 4, 4)
-ChangePinName(CONN8, 2, 2)
+# Start of element J3
+ChangePinName(J3, 8, I7)
+ChangePinName(J3, 7, I6)
+ChangePinName(J3, 6, I5)
+ChangePinName(J3, 5, I4)
+ChangePinName(J3, 4, I3)
+ChangePinName(J3, 3, I2)
+ChangePinName(J3, 2, I1)
+ChangePinName(J3, 1, I0)
 
-# Start of element CONN7
-ChangePinName(CONN7, 8, I7)
-ChangePinName(CONN7, 7, I6)
-ChangePinName(CONN7, 6, I5)
-ChangePinName(CONN7, 5, I4)
-ChangePinName(CONN7, 4, I3)
-ChangePinName(CONN7, 3, I2)
-ChangePinName(CONN7, 2, I1)
-ChangePinName(CONN7, 1, I0)
+# Start of element J2
+ChangePinName(J2, 8, I7)
+ChangePinName(J2, 7, I6)
+ChangePinName(J2, 6, I5)
+ChangePinName(J2, 5, I4)
+ChangePinName(J2, 4, I3)
+ChangePinName(J2, 3, I2)
+ChangePinName(J2, 2, I1)
+ChangePinName(J2, 1, I0)
 
-# Start of element CONN6
-ChangePinName(CONN6, 7, 7)
-ChangePinName(CONN6, 5, 5)
-ChangePinName(CONN6, 3, 3)
-ChangePinName(CONN6, 1, 1)
-ChangePinName(CONN6, 8, 8)
-ChangePinName(CONN6, 6, 6)
-ChangePinName(CONN6, 4, 4)
-ChangePinName(CONN6, 2, 2)
+# Start of element J1
+ChangePinName(J1, 8, I7)
+ChangePinName(J1, 7, I6)
+ChangePinName(J1, 6, I5)
+ChangePinName(J1, 5, I4)
+ChangePinName(J1, 4, I3)
+ChangePinName(J1, 3, I2)
+ChangePinName(J1, 2, I1)
+ChangePinName(J1, 1, I0)
 
 # Start of element U1
 ChangePinName(U1, 20, DIGIO7)
@@ -115,13 +109,3 @@ ChangePinName(U1, 4, GND)
 ChangePinName(U1, 3, 5V)
 ChangePinName(U1, 2, 3V3)
 ChangePinName(U1, 1, RESET)
-
-# Start of element CONN5
-ChangePinName(CONN5, 8, I7)
-ChangePinName(CONN5, 7, I6)
-ChangePinName(CONN5, 6, I5)
-ChangePinName(CONN5, 5, I4)
-ChangePinName(CONN5, 4, I3)
-ChangePinName(CONN5, 3, I2)
-ChangePinName(CONN5, 2, I1)
-ChangePinName(CONN5, 1, I0)
