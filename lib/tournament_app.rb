c74f078bf5ebc7c0d8370d42c2cfebc71206c4e6
Evan Farrar <evanfarrar@gmail.com>
2008-11-24 10:45:56 -0600

    Prettied up the tournament window slightly.

--- lib/tournament_app.rb
+++ lib/tournament_app.rb
@@ -14,7 +14,7 @@ Shoes.app(:title => TITLE, :width => 800, :height => 600) do
   end
 
   def delete_race(race)
-    image(20, 20, {:top => 8, :left => 250}) do
+    image(20, 20, {:top => 8, :left => 365}) do
       delete_button
       click do
         @tournament.matches.delete(race)
@@ -60,7 +60,7 @@ Shoes.app(:title => TITLE, :width => 800, :height => 600) do
   end
 
   def redblue(race)
-    image(20, 20, {:top => 8, :left => 150}) do
+    image(20, 20, {:top => 8, :left => 340}) do
       fill eval(BIKES[0])
       rect(:top => 0, :left => 0, :height => 15, :width => 7)
       fill eval(BIKES[1])
@@ -72,21 +72,21 @@ Shoes.app(:title => TITLE, :width => 800, :height => 600) do
     end 
   end
 
-  background white
+  background black
   @tournament = Tournament.new(RACE_DISTANCE)
  
   def list_racers
     flow do
-      flow(:width => 115) { para 'Name' }
-      flow(:width => 50) { para 'Wins' }
-      flow(:width => 25) { para 'Best' }
+      flow(:width => 115) { para 'Name', :stroke => ivory }
+      flow(:width => 50) { para 'Wins', :stroke => ivory }
+      flow(:width => 25) { para 'Best', :stroke => ivory }
     end
     @tournament.racers.compact.each do |racer|
       flow do
-        border black
-        flow(:width => 115) { para racer.name }
-        flow(:width => 50) { para racer.wins, " / ", racer.races }
-        flow(:width => 25) { para racer.best_time, "s" unless racer.best_time == Infinity }
+        border gray(0.65) 
+        flow(:width => 115) { para racer.name, :stroke => ivory }
+        flow(:width => 50) { para racer.wins, " / ", racer.races, :stroke => ivory }
+        flow(:width => 25) { para racer.best_time, "s", :stroke => ivory unless racer.best_time == Infinity }
         add_to_race racer
         delete_racer racer
       end
@@ -104,18 +104,20 @@ Shoes.app(:title => TITLE, :width => 800, :height => 600) do
   end
  
   def list_matches
-    border black
-    title "Matches"
+    background gray(0.10), :curve => 14
+    border gray(0.65), :curve => 14, :strokewidth => 3
+    title "Matches", :stroke => ivory
     @tournament.matches.each do |match|
       flow(:margin => 5) do
-        background lightgrey
+        background gray(0.3)
         border black
-        flow(:width => 180) do
+        flow(:width => 270) do
           # FIXME THIS IS HORRENDOUS
           para((span(match.racers[0].name+" ", :stroke => eval(BIKES[0])) if match.racers[0]),
                (span(match.racers[1].name+" ", :stroke => eval(BIKES[1])) if match.racers[1]),
                (span(match.racers[2].name+" ", :stroke => eval(BIKES[2])) if match.racers[2]),
-               (span(match.racers[3].name+" ", :stroke => eval(BIKES[3])) if match.racers[3]))
+               (span(match.racers[3].name+" ", :stroke => eval(BIKES[3])) if match.racers[3]),
+               :weight => "bold")
         end
         button("race")do
           race_window match, @tournament
@@ -141,17 +143,18 @@ Shoes.app(:title => TITLE, :width => 800, :height => 600) do
     @racer_list.clear {list_racers}
   end
  
-  stack(:width => 380, :margin => 5) do
-    border black
-    title "Racers"
+  stack(:width => 380, :margin => 5, :curve => 14) do
+    background gray(0.15), :curve => 14
+    border gray(0.65), :curve => 14, :strokewidth => 3
+    title "Racers", :stroke => ivory
     @racer_list = stack { list_racers }
-    flow do
+    flow(:margin => 8) do
       @racer_name = edit_line "enter name", :width => 110
       create_racer
     end
   end
  
-  @matches = stack(:width => 290, :margin => 5) do
+  @matches = stack(:width => 800 - gutter() - 380, :margin => 5, :curve => 14) do
     list_matches
   end
  
