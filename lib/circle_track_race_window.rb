b63c35a8cd9b5b7f61364c61a5a915f73516d408
toothrot <scissorjammer@gmail.com>
2009-01-28 21:02:18 -0600

    a lot cleaner

--- lib/circle_track_race_window.rb
+++ lib/circle_track_race_window.rb
@@ -1,28 +1,11 @@
 require 'lib/setup.rb'
 
-class RacePresenter
-  def initialize(shoes_instance, distance, progress, progress_2, race, sensor, bikes, best_time)
-    @shoes_instance = shoes_instance
-    @bar_size = 800-2*60-6
-    @race_distance = distance
-    @race = race
-    @progress, @progress_2 = progress, progress_2
-    @sensor = sensor
-    @continue = true
-    @bikes = bikes
-    @best_time = best_time
-  end
-
-end
-
 module RaceWindow
 
-  def race_window(match, tournament=nil)
+  def race_window(race, tournament=nil)
     window :title => TITLE, :width => 800, :height => 600 do
-      race_distance, sensor, title = $RACE_DISTANCE, SENSOR, TITLE
-      @race, @sensor, @race_distance = match, sensor, race_distance
-      bikes = BIKES
-      @continue = true
+      @race_distance, @sensor, title = $RACE_DISTANCE, SENSOR, TITLE
+      @race = race
 
       background lawngreen 
 
@@ -85,13 +68,15 @@ module RaceWindow
 
       @start = button("Start Race") do
         @start.hide
-        match.racers.each{|racer| racer.ticks = 0 }
-        match.racers.each{|racer| racer.finish_time = nil }
-        sensor.start
+        @race.racers.each{|racer| racer.ticks = 0 }
+        @race.racers.each{|racer| racer.finish_time = nil }
+        @sensor.start
 
         @race_animation = animate(14) do
-          if @continue
-            draw_race 
+          draw_race 
+          if @race.complete?
+            @race_animation.stop
+            @sensor.stop
           end
         end
       end
@@ -106,52 +91,45 @@ module RaceWindow
         hide
       end
 
-  def draw_race 
-    @race.racers.size.times do |i|
-      @race.racers[i].ticks = @sensor.racers[i].size
-    end
+      def draw_race 
+        @race.racers.size.times do |i|
+          @race.racers[i].ticks = @sensor.racers[i].size
+        end
+        racer_0_arclength = Shoes::TWO_PI*(percent_complete(@race.racers[0]))
+        racer_1_arclength = Shoes::TWO_PI*(percent_complete(@race.racers[1]))
 
-    @progress.clear do
-      fill red
-      stroke red
-      strokewidth 200
-      arc self.width/2, self.height/2, 
-        450, 350, 
-        0, Shoes::TWO_PI*(percent_complete(@race.racers[0]))
-      stroke white
-      fill white
-      arc self.width/2, self.height/2, 
-        450, 350, 
-        Shoes::TWO_PI*(percent_complete(@race.racers[0])), 0
-    end
+        @progress.clear do
+          fill red
+          stroke red
+          strokewidth 200
+          arc self.width/2, self.height/2, 
+            450, 350, 
+            0, racer_0_arclength
+          stroke white
+          fill white
+          arc self.width/2, self.height/2, 
+            450, 350, 
+            racer_0_arclength, 0
+        end
 
-    @progress_2.clear do
-      fill blue
-      stroke blue
-      strokewidth 200
-      arc self.width/2, self.height/2, 
-        450, 350, 
-        0, Shoes::TWO_PI*(percent_complete(@race.racers[1]))
-      stroke white
-      fill white
-      arc self.width/2, self.height/2, 
-        450, 350, 
-        Shoes::TWO_PI*(percent_complete(@race.racers[1])), 0
-    end
+        @progress_2.clear do
+          fill blue
+          stroke blue
+          strokewidth 200
+          arc self.width/2, self.height/2, 
+            450, 350, 
+            0, racer_1_arclength
+          stroke white
+          fill white
+          arc self.width/2, self.height/2, 
+            450, 350, 
+            racer_1_arclength, 0
+        end
+      end
 
-    if @race.complete?
-      @sensor.stop
-      @continue = false
-    end
-  end
-
-  def percent_complete(racer)
-    [1.0, ((racer.ticks * racer.roller_circumference) || 0)/@race_distance.to_f].min
-  end
-  
-  def ticks_in_race
-    (@race_distance/@race.racers[0].roller_circumference)
-  end
+      def percent_complete(racer)
+        [1.0, ((racer.ticks * racer.roller_circumference) || 0)/@race_distance.to_f].min
+      end
     end
   end
 end
